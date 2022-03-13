9b256e1ca349c4f58f2a6dc743f7ccec41dfc38b
santiagortiz1710 <98198468+santiagortiz1710@users.noreply.github.com>
2022-03-12 22:44:41 -0500

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
 import { PerfilEmpleadorComponent } from './Components/perfil-empleador/perfil-empleador.component';
 
 
 const routes: Routes = [
   {path: 'Perfilempleador', component: PerfilEmpleadorComponent },
+  {path: '', component:HomeComponent},
 ];
 
 @NgModule({
@@ -12,3 +14,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+  
